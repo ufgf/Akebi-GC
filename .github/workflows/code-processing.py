635fe9c38c1a6763f704a104e64359d18bdb42e8
ufgf <29230208+ufgf@users.noreply.github.com>
2022-09-22 13:08:53 +0300

    Update code-processing.py

--- .github/workflows/code-processing.py
+++ .github/workflows/code-processing.py
@@ -1,11 +1,10 @@
 # pylint: disable=C0114:missing-docstring, C0301:line-too-long, C0103:invalid-name, R1727:condition-evals-to-constant, W1514:unspecified-encoding, W0703:broad-except
 
-# don't blame me for such low-quality code ¯\_(ツ)_/¯
-print("- Making some changes to sources...")
+print("- Making some small changes to sources...")
 
 try:
     with open('cheat-library/cheat-library.vcxproj', 'r+', newline="") as f:
-        print("— commenting out CustomBuildStep sections... ", end="")
+        print("- commenting out CustomBuildStep sections... ", end="")
         lines = f.readlines()
         f.seek(0)
 
@@ -15,5 +14,6 @@ try:
                 f.write(line)
 
 except Exception as e:
-    print(f"unsuccessful, reason: {e}")
-print("done")
+    print(f"unsuccessful ({e.__class__}: {e}).")
+
+print("done.")
